fffa4f65b17249064896d356da81d49773b9fedf
Randall C. O'Reilly <randy.oreilly@colorado.edu>
2019-05-10 04:44:20 -0600

    add doc

---
+++ eplot/doc.go
@@ -0,0 +1,11 @@
+// Copyright (c) 2019, The Emergent Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+/*
+Package eplot provides an interactive, graphical plotting utility for etable data.
+
+It also provides various utilities to simplify the plotting of data from
+etable.Table data tables, using the gonum/plot package.
+*/
+package eplot
