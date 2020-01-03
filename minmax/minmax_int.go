8d6e80cba0721201e4a482b0fe76a683073780bf
Randall C. O'Reilly <randy.oreilly@colorado.edu>
2020-01-03 00:25:07 -0800

    added minmax.Int

--- minmax/minmax_int.go
+++ minmax/minmax_int.go
@@ -6,66 +6,66 @@ package minmax
 
 import "math"
 
-// F32 represents a min / max range for float32 values.
+// Int represents a min / max range for int values.
 // Supports clipping, renormalizing, etc
-type F32 struct {
-	Min float32
-	Max float32
+type Int struct {
+	Min int
+	Max int
 }
 
 // Set sets the min and max values
-func (mr *F32) Set(min, max float32) {
+func (mr *Int) Set(min, max int) {
 	mr.Min, mr.Max = min, max
 }
 
 // SetInfinity sets the Min to +MaxFloat, Max to -MaxFloat -- suitable for
 // iteratively calling Fit*InRange
-func (mr *F32) SetInfinity() {
-	mr.Min, mr.Max = math.MaxFloat32, -math.MaxFloat32
+func (mr *Int) SetInfinity() {
+	mr.Min, mr.Max = math.MaxInt64, -math.MaxInt64
 }
 
 // IsValid returns true if Min <= Max
-func (mr *F32) IsValid() bool {
+func (mr *Int) IsValid() bool {
 	return mr.Min <= mr.Max
 }
 
 // InRange tests whether value is within the range (>= Min and <= Max)
-func (mr *F32) InRange(val float32) bool {
+func (mr *Int) InRange(val int) bool {
 	return ((val >= mr.Min) && (val <= mr.Max))
 }
 
 // IsLow tests whether value is lower than the minimum
-func (mr *F32) IsLow(val float32) bool {
+func (mr *Int) IsLow(val int) bool {
 	return (val < mr.Min)
 }
 
 // IsHigh tests whether value is higher than the maximum
-func (mr *F32) IsHigh(val float32) bool {
+func (mr *Int) IsHigh(val int) bool {
 	return (val > mr.Min)
 }
 
 // Range returns Max - Min
-func (mr *F32) Range() float32 {
+func (mr *Int) Range() int {
 	return mr.Max - mr.Min
 }
 
 // Scale returns 1 / Range -- if Range = 0 then returns 0
-func (mr *F32) Scale() float32 {
+func (mr *Int) Scale() float32 {
 	r := mr.Range()
 	if r != 0 {
-		return 1 / r
+		return 1 / float32(r)
 	}
 	return 0
 }
 
 // Midpoint returns point halfway between Min and Max
-func (mr *F32) Midpoint() float32 {
-	return 0.5 * (mr.Max + mr.Min)
+func (mr *Int) Midpoint() float32 {
+	return 0.5 * float32(mr.Max+mr.Min)
 }
 
-// FitInRange adjusts our Min, Max to fit within those of other F32
+// FitInRange adjusts our Min, Max to fit within those of other Int
 // returns true if we had to adjust to fit.
-func (mr *F32) FitInRange(oth F32) bool {
+func (mr *Int) FitInRange(oth Int) bool {
 	adj := false
 	if oth.Min < mr.Min {
 		mr.Min = oth.Min
@@ -80,7 +80,7 @@ func (mr *F32) FitInRange(oth F32) bool {
 
 // FitValInRange adjusts our Min, Max to fit given value within Min, Max range
 // returns true if we had to adjust to fit.
-func (mr *F32) FitValInRange(val float32) bool {
+func (mr *Int) FitValInRange(val int) bool {
 	adj := false
 	if val < mr.Min {
 		mr.Min = val
@@ -94,17 +94,17 @@ func (mr *F32) FitValInRange(val float32) bool {
 }
 
 // NormVal normalizes value to 0-1 unit range relative to current Min / Max range
-func (mr *F32) NormVal(val float32) float32 {
-	return (val - mr.Min) * mr.Scale()
+func (mr *Int) NormVal(val int) float32 {
+	return float32(val-mr.Min) * mr.Scale()
 }
 
 // ProjVal projects a 0-1 normalized unit value into current Min / Max range (inverse of NormVal)
-func (mr *F32) ProjVal(val float32) float32 {
-	return mr.Min + (val * mr.Range())
+func (mr *Int) ProjVal(val float32) float32 {
+	return float32(mr.Min) + (val * float32(mr.Range()))
 }
 
 // ClipVal clips given value within Min / Max rangee
-func (mr *F32) ClipVal(val float32) float32 {
+func (mr *Int) ClipVal(val int) int {
 	if val < mr.Min {
 		return mr.Min
 	}
@@ -115,7 +115,7 @@ func (mr *F32) ClipVal(val float32) float32 {
 }
 
 // ClipNormVal clips then normalizes given value within 0-1
-func (mr *F32) ClipNormVal(val float32) float32 {
+func (mr *Int) ClipNormVal(val int) float32 {
 	if val < mr.Min {
 		return 0
 	}
