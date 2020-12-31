fb36e0f461a97409e3734e99fd3605b310c96a77
Randall C. O'Reilly <randy.oreilly@colorado.edu>
2020-12-31 02:44:13 -0800

    v1.0.20 release -- version.go updated

--- etable/version.go
+++ etable/version.go
@@ -3,8 +3,8 @@
 package etable
 
 const (
-	Version     = "v1.0.19"
-	GitCommit   = "013b423" // the commit JUST BEFORE the release
-	VersionDate = "2020-12-30 09:49" // UTC
+	Version     = "v1.0.20"
+	GitCommit   = "a31fa89" // the commit JUST BEFORE the release
+	VersionDate = "2020-12-31 10:44" // UTC
 )
 
