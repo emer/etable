a11dd0a1505cd189f85d0aae6c0c74377ab988be
Randall C. O'Reilly <randy.oreilly@colorado.edu>
2023-03-28 22:54:15 -0700

    v1.1.20 release -- version.go updated

--- etable/version.go
+++ etable/version.go
@@ -3,7 +3,7 @@
 package etable
 
 const (
-	Version     = "v1.1.19"
-	GitCommit   = "162d5f7"          // the commit JUST BEFORE the release
-	VersionDate = "2023-03-25 12:02" // UTC
+	Version     = "v1.1.20"
+	GitCommit   = "2a880dc"          // the commit JUST BEFORE the release
+	VersionDate = "2023-03-29 05:54" // UTC
 )
