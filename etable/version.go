84b95b916aa55a01af652676334061ce7461912f
Kai O'Reilly <koreilly5297@gmail.com>
2023-12-12 16:16:05 -0800

    updated version to v2.0.0-dev0.0.6

--- etable/version.go
+++ etable/version.go
@@ -4,9 +4,9 @@ package etable
 
 const (
 	// Version is the version of this package being used
-	Version = "v2.0.0-dev0.0.5"
+	Version = "v2.0.0-dev0.0.6"
 	// GitCommit is the commit just before the latest version commit
-	GitCommit = "6937c22"
+	GitCommit = "095c791"
 	// VersionDate is the date-time of the latest version commit in UTC (in the format 'YYYY-MM-DD HH:MM', which is the Go format '2006-01-02 15:04')
-	VersionDate = "2023-12-12 20:43"
+	VersionDate = "2023-12-13 00:16"
 )
